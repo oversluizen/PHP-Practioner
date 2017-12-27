6ba13e43b0593ad56b02c32d53186ed077b72c0c
oversluizen <jhoversluizen@gmail.com>
2017-12-27 21:57:20 +0100

    Lesson 5 Separate PHP Logic from Presentation

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header{
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+<?php
 
-    <header>
-        <h1>
-            <?= "Hello, ". htmlspecialchars($_GET['name']); ?>
-        </h1>
-    </header>
+$greeting = 'Hello, World';
 
-</body>
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>
+            <?= $greeting; ?>
+        </h1>
+    </header>
+
+</body>
+</html>
